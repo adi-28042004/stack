1752b49b76a45dd36b60766e20f36688810d15d3
adi-28042004 <113676573+adi-28042004@users.noreply.github.com>
2023-04-14 00:22:06 +0530

    Add files via upload

---
+++ stack/valid parenthesis.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> st;
+        for(auto ch:s){
+            if(st.empty()){
+                st.push(ch);
+            }
+            else if((st.top()=='(' && ch==')' ) || (st.top()=='{' && ch=='}') || (st.top()=='[' && ch==']'))
+            {
+                st.pop();
+            }
+            else
+            {
+                st.push(ch);
+            }
+        }
+        if(st.size()==0)
+            return true;
+        return false;
+
+    }   
+        
+};
